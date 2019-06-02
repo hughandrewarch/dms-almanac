450037cc99855717130498bdfc0b4bf5f6a3bb31
Drew Archibald <hughandrewarch@gmail.com>
2019-06-02 19:07:55 -0400

    update fetchTownList action to make api call to Sage

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -5,10 +5,16 @@ export function townMiddleware({ dispatch }) {
   return function (next) {
     return function (action) {
       //TODO 1 : use an api call to get (once i make a backend),
-      //     1b: shouldn't need parse int at this point
       switch (action.type) {
         case FETCH_TOWN_LIST:
-          return dispatch(receiveTownList(townList))
+            //TODO move call somewhere nice
+            fetch('http://localhost:8080/towns')
+                .then(res => res.json())
+                .then((data) => {
+                  dispatch(receiveTownList(data))
+                })
+                .catch(console.log)
+            break
         case FETCH_TOWN:
           const townId = parseInt(action.payload)
           let town = allFullTowns.filter(town => town.id === townId)[0]
@@ -18,20 +24,14 @@ export function townMiddleware({ dispatch }) {
 
           let people = peopleList.filter(person => person.townId === townId)
           town.people = people
-          return dispatch(receiveTown(town))
+          dispatch(receiveTown(town))
+          break;
         default:
       }
       return next(action)
     }
   }
 }
-
-const townList = [
-  { id: 1, name: 'Hughan' },
-  { id: 2, name: 'Amberlea' },
-  { id: 3, name: 'Roseport' },
-  { id: 4, name: 'Elkshorn' }
-]
 
 const allFullTowns = [
   {
