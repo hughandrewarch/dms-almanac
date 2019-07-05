14fed5b2b24e63861772cbba021000aedf24eb38
Drew Archibald <hughandrewarch@gmail.com>
2019-07-05 10:32:44 -0400

    add create to domain place repository

--- backend/magic/src/main/kotlin/app/adapters/DomainPlaceRepository.kt
+++ backend/magic/src/main/kotlin/app/adapters/DomainPlaceRepository.kt
@@ -2,11 +2,15 @@ package app.adapters
 
 import almanac.services.PlaceService
 import almanac.models.Place
+import almanac.models.PlaceType
 import almanac.ports.api.PlaceRepository
 import org.springframework.stereotype.Component
 
 @Component
 class DomainPlaceRepository(private val service: PlaceService): PlaceRepository {
+    override fun create(name: String, description: String, type: PlaceType): Place {
+        return service.create(name, description, type)
+    }
 
     override fun findAll(settlementId: Long): List<Place> {
         return service.findPlaces(settlementId)
--- backend/weave/src/main/kotlin/almanac/ports/api/PlaceRepository.kt
+++ backend/weave/src/main/kotlin/almanac/ports/api/PlaceRepository.kt
@@ -1,7 +1,9 @@
 package almanac.ports.api
 
 import almanac.models.Place
+import almanac.models.PlaceType
 
 interface PlaceRepository {
     fun findAll(settlementId: Long): List<Place>
+    fun create(name: String, description: String, type: PlaceType): Place
 }
