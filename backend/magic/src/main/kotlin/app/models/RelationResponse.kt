ba739328437803f73624b046d26eb1fe4d08b7c2
hughandrewarch <hughandrewarch@gmail.com>
2020-04-27 14:39:55 -0400

    clean up RelationResponse object

--- backend/magic/src/main/kotlin/app/models/RelationResponse.kt
+++ backend/magic/src/main/kotlin/app/models/RelationResponse.kt
@@ -1,49 +1,24 @@
 package app.models
 
-data class RelationResponse1(
-        val settlementRelations: Map<Long, Relation>
-        //
+data class RelationResponse(
+        val relations: List<Relation>
 )
 
-data class PersonRelation(
-        val person: Long,
-        val related: Long,
-        val type: String,
-        val description: String?
+//Todo maybe make domain object
+data class Relation(
+        val left: Long,
+        val right: Long,
+        val relation: String
 )
-
 /*
-    RelationType:
-        id: ...,
-        type: SettlementPerson
-
-    Relation:
-        left: id, (object)
-        right: id, (object)
-        relation: id, (relation)
-        description: text (maybe type?)
-
-*/
-
-/*
-    Relation
-        Id
-        Type
-        description
-
     State
     Relations {
         PersonRelations {
             id: [{
                 relation:
                 relationId:
-                type:
             }, ...
             ]
         },
     }
-
  */
-
-//TODO make domain object?
-data class Relation(val id: Long)
