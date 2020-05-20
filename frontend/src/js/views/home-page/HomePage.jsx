ff810478d95fc06bbd7e1eae6a7c9738f89ec5f4
hughandrewarch <hughandrewarch@gmail.com>
2020-05-20 15:22:54 -0400

    add lists to homepage

--- frontend/src/js/views/home-page/HomePage.jsx
+++ frontend/src/js/views/home-page/HomePage.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PeopleSelector from "../../selectors/PeopleSelector"
+import SettlementsSelector from "../../selectors/SettlementsSelector"
+import PersonTable from "../components/person/PersonTable"
+import SettlementTable from "../components/settlement/SettlementTable"
+
+const mapStateToProps = (state, props) => {
+    return {
+        settlements: SettlementsSelector.selectAll(state),
+        people: PeopleSelector.selectAll(state)
+    }
+}
 
 class HomePage extends React.Component {
 
-    settlement1() {
-        this.props.history.push('/settlement/1')
+    renderSettlements() {
+        const { settlements, history } = this.props
+
+        if(this.props.settlements) {
+            return(
+                <SettlementTable settlements={settlements} history={history} />
+            )
+        }
     }
 
-    settlement2() {
-        this.props.history.push('/settlement/2')
+    renderPeople() {
+        const { people, history } = this.props
+
+        if(this.props.people) {
+            return(
+                <PersonTable people={people} history={history}/>
+            )
+        }
     }
 
     render() {
         return (
-        <div>
-            <div>HOME PAGE</div>
-            <button onClick={this.settlement1.bind(this)}>Settlement 1</button>
-            <button onClick={this.settlement2.bind(this)}>Settlement 2</button>
-        </div>
-        );
+            <div>
+                <div>HOME PAGE</div>
+                <h2>Settlements</h2>
+                <div>{this.renderSettlements()}</div>
+                <h2>People</h2>
+                <div>{this.renderPeople()}</div>
+            </div>)
     }
 }
 
 //Will be connecting later
-export default connect(null, null)(HomePage)
+export default connect(mapStateToProps, null)(HomePage)
