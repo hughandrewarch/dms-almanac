2d32a1c3358294bfa077ad223c3ecb0b4aaa6173
hughandrewarch <hughandrewarch@gmail.com>
2020-06-18 09:44:15 -0300

    merge PersonCreateForm and PersonCreateFormFields

--- frontend/src/js/views/components/person/PersonCreateForm.jsx
+++ frontend/src/js/views/components/person/PersonCreateForm.jsx
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import PersonCreateFormFields from "js/views/components/person/PersonCreateFormFields"
 import PropTypes from "prop-types"
 import PersonActions from "js/actions/PersonActions"
+import TextFormField from "js/views/components/TextFormField"
+import TextAreaFormField from "js/views/components/TextAreaFormField"
+import RaceSelect from "js/views/components/RaceSelect"
+import SubRaceSelect from "js/views/components/SubRaceSelect"
 
 const mapDispatchToProps = (dispatch) => {
     return {
@@ -22,24 +26,11 @@ class PersonCreateForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
-      description: '',
+        name: { value: '' },
+        description: { value: '' },
+        race: { value: '' },
+        subrace: { value: '' }
     }
-  }
-
-  handleChange = (values) => {
-    this.setState({
-      name: values.name,
-      description: values.description,
-      race: values.race,
-      subrace: values.subrace,
-    })
-  }
-
-  hasErrors = () => {
-    const { name, description, race } = this.state
-
-    return name.errors || description.errors || race.errors
   }
 
   handleSubmit = () => {
@@ -64,10 +55,84 @@ class PersonCreateForm extends React.Component {
     this.props.onCancel()
   }
 
+  handleChange = event => {
+      const name = event.target.name
+      const value = event.target.value
+      const errors = event.target.errors
+
+      this.setState({
+        [name]: {
+          ...this.state[name],
+          value: value,
+          errors: errors
+        }
+      })
+  }
+
+  hasErrors = () => {
+    const { name, description, race } = this.state
+
+    return name.errors || description.errors || race.errors
+  }
+
+  showError(name) {
+    return this.state[name].errors && this.state.showErrors
+  }
+
+  renderNameFormField() {
+    let classError = this.showError("name") ? "t-error" : ""
+
+    return (
+        <TextFormField
+          className={classError}
+          name="name"
+          value={this.state.name.value}
+          onChange={this.handleChange}
+          isRequired
+          />
+    )
+  }
+
+  renderDescriptionFormField() {
+    return (
+        <TextAreaFormField
+          name="description"
+          value={this.state.description.value}
+          onChange={this.handleChange}/>
+    )
+  }
+
+  renderRaceFormField() {
+      return (
+        <RaceSelect
+            name="race"
+            value={this.state.race.value}
+            onChange={this.handleChange}/>
+      )
+  }
+
+  renderSubRaceFormField() {
+      const { race, subrace } = this.state
+
+      return (
+          <SubRaceSelect
+              name="subrace"
+              race={race.value}
+              value={subrace.value}
+              onChange={this.handleChange}/>
+      )
+  }
+
+  //TODO consider splitting when setting up css
+  //     it might be cleaner to have the manipulation
+  //     split from the display
   render() {
     return (
       <div>
-        <PersonCreateFormFields onChange={this.handleChange} showErrors={this.state.showErrors}/>
+        {this.renderNameFormField()}
+        {this.renderDescriptionFormField()}
+        {this.renderRaceFormField()}
+        {this.renderSubRaceFormField()}
         <button onClick={this.handleSubmit}>Submit</button>
         <button onClick={this.handleCancel}>Cancel</button>
       </div>
--- frontend/src/js/views/components/settlement/SettlementCreateForm.jsx
+++ frontend/src/js/views/components/settlement/SettlementCreateForm.jsx
@@ -54,7 +54,6 @@ class SettlementCreateForm extends React.Component {
     }
     create(settlement)
         .then(onSubmit)
-
   }
 
   handleCancel = () => {
